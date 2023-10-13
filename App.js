8c82202bec2152082e0f9ad03e7ee58d71e5a7b3
Galip Sen <galipshen@gmail.com>
2023-10-13 17:15:24 +0300

    Header List and Button imported and used

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// components
+import Header from './src/components/Header';
+import List from './src/components/List';
+import Button from './src/components/Button';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Header/>
+      <List/>
+      <Button/>
       <StatusBar style="auto" />
     </View>
   );

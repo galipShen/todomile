23600c554af7da46688c8f9f474623cdfe353237
Galip Sen <galipshen@gmail.com>
2023-10-13 17:17:05 +0300

    StatusBar replaced with react-native one's

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 // components
 import Header from './src/components/Header';
 import List from './src/components/List';
@@ -20,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
